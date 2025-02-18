08fb962767befc97cfa790f16fe35776de8f8ad5
Kounex <r.schramowski@googlemail.com>
2025-02-18 03:27:25 +0100

    changed default git config and theme

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,7 +3,7 @@
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="muse"
 DEFAULT_USER=kounex
 
 typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
